95cb3c40f563c00a50ef6867920bd5bbda2b1d22
kavin prasath <kavinprasath67175@gmail.com>
2024-07-31 08:46:46 +0530

    commit

--- src/main/java/com/example/SpringDemoProject/SpringDemoProjectApplication.java
+++ src/main/java/com/example/SpringDemoProject/SpringDemoProjectApplication.java
@@ -9,6 +9,7 @@ public class SpringDemoProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoProjectApplication.class, args);
 		System.out.println("test");
+		System.out.println("test2");
 	}
 
 }
